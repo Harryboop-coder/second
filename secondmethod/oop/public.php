bf2998f29417416ab9d3b55c95575a4377fde543
Harryboop-coder <ikemdickson970@gmail.com>
2025-09-03 11:09:58 -0800

    added oop

--- secondmethod/oop/public.php
+++ secondmethod/oop/public.php
@@ -1,17 +1,10 @@
 <?php
 class Circle {
     public $radius;
-    public $pi = 3.142;  
+    public $pi = 3.142;
 
     public function arcLength($angle) {
         return ($angle / 360) * (2 * $this->pi * $this->radius);
-    }
-
-    
-    public function chordLength($angle) {
-
-        $radians = deg2rad($angle / 2);
-        return 2 * $this->radius * sin($radians);
     }
 }
 
@@ -21,7 +14,5 @@ $circle->radius = 7;
 
 $arc = $circle->arcLength(70);
 
-$chord = $circle->chordLength(70);
-
-echo "Minor Arc PQ = " . round($arc, 1) . " cm<br>";
+echo "Minor Arc PQ = " . round($arc, 1) . " cm";
 ?>
